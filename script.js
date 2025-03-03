a33ca7c27c2da5a6c1824335af3c8681714ac67f
Jess179 <jamenosa.rdpi@gmail.com>
2025-03-03 10:04:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-// Add interactivity to the stars
-const stars = document.querySelectorAll('.star');
-stars.forEach(star => {
-  star.addEventListener('click', () => {
-    const message = star.getAttribute('data-message');
-    alert(message);
-  });
-});
-
-// Crush button reveal
-const crushButton = document.getElementById('crushButton');
-const secretMessage = document.getElementById('secretMessage');
-
-crushButton.addEventListener('click', () => {
+function showMessage() {
+  const secretMessage = document.getElementById('secretMessage');
   secretMessage.classList.remove('hidden');
-});
+}
