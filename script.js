5f1b396b64c9ab2e9af9d0d72efeb1394592b114
Jess179 <jamenosa.rdpi@gmail.com>
2025-03-03 11:46:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,13 @@ let isAnimationVisible = false;
 
 function toggleAnimation() {
   const animationContainer = document.getElementById('animation-container');
-  const walkingGirl = document.getElementById('walking-girl');
-
   if (!isAnimationVisible) {
     // Show the animation
     animationContainer.classList.remove('hidden');
-    walkingGirl.style.animation = 'walkIn 2s forwards';
     isAnimationVisible = true;
   } else {
     // Hide the animation
-    walkingGirl.style.animation = 'walkOut 2s forwards';
-    setTimeout(() => {
-      animationContainer.classList.add('hidden');
-    }, 2000); // Wait for the animation to finish
+    animationContainer.classList.add('hidden');
     isAnimationVisible = false;
   }
 }
